2e8047d40694089032aad553f4499b5b5991ef24
Daniel <dls.2000@hotmail.com>
2021-10-25 15:37:40 -0300

    Primeiro MVC ( Departamento)

---
+++ WebSalesMVC/Controllers/DepartmentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSalesMVC.Models;
+
+namespace WebSalesMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department {Id = 1 , Name = "Mobile" });
+            list.Add(new Department { Id = 2, Name = "Sound" });
+            list.Add(new Department { Id = 3, Name = "Video" });
+
+            return View(list);
+        }
+    }
+}
