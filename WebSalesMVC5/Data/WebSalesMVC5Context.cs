73704067259ff5b3143b963fece4527fd29970e6
Daniel <dls.2000@hotmail.com>
2021-10-26 20:34:09 -0300

    Add project files.

---
+++ WebSalesMVC5/Data/WebSalesMVC5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSalesMVC5.Models;
+
+namespace WebSalesMVC5.Data
+{
+    public class WebSalesMVC5Context : DbContext
+    {
+        public WebSalesMVC5Context (DbContextOptions<WebSalesMVC5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebSalesMVC5.Models.Department> Department { get; set; }
+    }
+}
